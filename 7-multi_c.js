70b4ac803391d2670426a21367661526f06be595
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-11-06 05:08:21 +0200

    Task 7. I love C

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+const { argv } = require('node:process');
+
+let x = Number(argv[2]);
+
+if (isNaN(x)) {
+    console.log("Missing number of occurrences");
+} else {
+    for (let i = 0 ; i<x; i++){
+        console.log("C is fun");
+    }
+}
